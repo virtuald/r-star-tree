845ae41c122b4302e5a7cc53614c33f4c077db0a
Dustin Spicuzza <dustin@virtualroadside.com>
2016-07-03 12:04:09 -0400

    Update license to LGPL, add readme

--- RStarTree.h
+++ RStarTree.h
@@ -1,19 +1,20 @@
 /*
  *  Copyright (c) 2008 Dustin Spicuzza <dustin@virtualroadside.com>
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of version 2 of the GNU General Public License
- *  as published by the Free Software Foundation.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of version 2.1 of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
  
 /*
  *	This is intended to be a templated implementation of an R* Tree, designed
